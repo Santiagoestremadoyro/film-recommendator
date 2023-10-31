0e3cd562ccec8c91dc06765538df308cc01db563
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-10-31 23:25:04 +0100

    update

---
+++ src/streamlit/streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-decoration: underline;'>Palmer Archipelago (Antarctica)</h1>", unsafe_allow_html=True)
+st.subheader("Discover the three types of species that inhabit this wonderful archipelago and their characteristics")
+
+st.markdown("***")
+
+st.image('./data/palmer.png')
+
+
+
+
+
