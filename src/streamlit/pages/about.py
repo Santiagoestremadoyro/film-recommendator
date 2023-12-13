95649394be806cdc40854c8902987b34144725d1
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-12-13 15:29:06 +0100

    stream

--- src/streamlit/pages/about.py
+++ src/streamlit/pages/about.py
@@ -1,11 +1,31 @@
 import streamlit as st
-from database import connection
 import os
 
 
-#conexion a la BD 
-conn = connection.Database(
-                os.getenv("MONGODB_URI"), os.getenv("DB_NAME"), os.getenv("DB_COLL"))
+
+contact_form = """
+<form action="https://formsubmit.co/little.big.whales@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
 
 
-st.markdow("<h1 style= 'text-decoration:'>Sign up to our communiy and hel us improve!</h1>", unsafe_allow_html=True)
+st.markdown(contact_form, unsafe_allow_html=True)
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
+st.header(":mailbox: Get In Touch With Me!")
+st.markdown(
+        '''
+           Do you want to propose us ideas or contribute in some way to our community?
+           Send us an email with your proposals/ideas and we will reply to you as soon as possible.
+        '''
+    )
--- src/streamlit/streamlit.py
+++ src/streamlit/streamlit.py
@@ -6,30 +6,9 @@ st.subheader("Discover new movies or series in our collection. Enter your three 
 st.button("Empecemos")
 st.slider("Cueantas recomendaciones quieres ver", 0, 50)
 
-st.header(":mailbox: Get In Touch With Me!")
-st.markdown(
-        '''
-           Do you want to propose us ideas or contribute in some way to our community?
-           Send us an email with your proposals/ideas and we will reply to you as soon as possible.
-        '''
-    )
-
-contact_form = """
-<form action="https://formsubmit.co/little.big.whales@gmail.com" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></textarea>
-     <button type="submit">Send</button>
-</form>
-"""
 
 
-st.markdown(contact_form, unsafe_allow_html=True)
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 
