24179a296940fa46fe52fb3a4b5c41a2944d4656
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-12-14 15:56:06 +0100

    streamlit5

--- src/streamlit/pages/about.py
+++ src/streamlit/pages/about.py
@@ -3,18 +3,9 @@ import os
 
 
 
-contact_form = """
-<form action="https://formsubmit.co/little.big.whales@gmail.com" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></textarea>
-     <button type="submit">Send</button>
-</form>
-"""
 
 
-st.markdown(contact_form, unsafe_allow_html=True)
+
 
 def local_css(file_name):
     with open(file_name) as f:
