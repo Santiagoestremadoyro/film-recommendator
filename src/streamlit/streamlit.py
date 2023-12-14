62b924459b1cad587dae8c5f4382b6a4a1b6e88b
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-12-14 15:43:08 +0100

    streamlit3

--- src/streamlit/streamlit.py
+++ src/streamlit/streamlit.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-decoration: underline;'>Film Recommendator!!</h1>", unsafe_allow_html=True)
+st.markdown(
+        '''
+                Discover the best movies, of any genre in our vast catalog of movies and find your new favorite movie/series!!
+        '''
+    )
+
 st.subheader("Discover new movies or series in our collection. Enter your three favorite movies/series")
 
 
-st.button("Empecemos")
 st.slider("Cueantas recomendaciones quieres ver", 0, 50)
 
+selectbox = st.selectbox("What genre would you like to see?", ["Action", "Romance", "Thriler"])
 
 
 
@@ -21,5 +27,6 @@ st.slider("Cueantas recomendaciones quieres ver", 0, 50)
 
 
 
-st.button("Empecemos")
-st.slider("Cueantas recomendaciones quieres ver", 0, 50)
+
+
+
