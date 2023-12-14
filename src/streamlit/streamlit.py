60db3784dc3f62e21dd3a1a7a847bae52aef3227
santiago <119969620+Santiagoestremadoyro@users.noreply.github.com>
2023-12-14 15:47:21 +0100

    Streamlit (#20)
    * chore: add API (#18)
    
    * recommUpd
    
    * organization
    
    * streamlit
    
    * update
    
    * update1
    
    * pages
    
    * streamlit
    
    * stream
    
    * streamlit3
    
    ---------
    
    Co-authored-by: Valglez <71898653+valglez@users.noreply.github.com>

--- src/streamlit/streamlit.py
+++ src/streamlit/streamlit.py
@@ -1,19 +1,16 @@
 import streamlit as st
 
 
+
 st.subheader("Discover new movies or series in our collection. Enter your three favorite movies/series")
 
 
-st.button("Empecemos")
 st.slider("Cueantas recomendaciones quieres ver", 0, 50)
 
 
+selectbox = st.selectbox("What genre would you like to see?", ["Action", "Romance", "Thriler"])
 
-st.markdown(contact_form, unsafe_allow_html=True)
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 
